0bbe47ccf84f85415e015751bcd66f92465e60d6
liudonghua123 <liudonghua123@users.noreply.github.com>
2024-12-09 07:50:14 +0000

    deploy: d42f23e778e1f6c5ea89bfa42d23eab7e8a72f69

--- assets/json-minify-fab6cf37.js
+++
@@ -1,5 +0,0 @@
-import{_ as a}from"./FormatTransformer.vue_vue_type_script_setup_true_lang-f1609130.js";import{l as r}from"./index-231c3e11.js";import{w as i}from"./defaults-4d6daddf.js";import{d as s,o as l,c as p}from"./index-9a786956.js";import"./TextareaCopyable-84be020e.js";import"./Copy-17f950e6.js";import"./Scrollbar-e4d13672.js";const u=`{
-	"hello": [
-		"world"
-	]
-}`,N=s({__name:"json-minify",setup(m){const t=o=>i(()=>JSON.stringify(r.parse(o),null,0),""),e=[{validator:o=>o===""||r.parse(o),message:"Provided JSON is not valid."}];return(o,f)=>{const n=a;return l(),p(n,{"input-label":"Your raw JSON","input-default":u,"input-placeholder":"Paste your raw JSON here...","output-label":"Minified version of your JSON","output-language":"json","input-validation-rules":e,transformer:t})}}});export{N as default};
